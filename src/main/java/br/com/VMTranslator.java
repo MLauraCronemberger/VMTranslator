039eecf99db7fd46a11300ce018503749411d003
Maria Laura <maria.cronemberger@ssp.ma.gov.br>
2026-06-03 15:56:31 -0300

    feat: add vmtranslator

---
+++ src/main/java/br/com/VMTranslator.java
@@ -0,0 +1,44 @@
+package br.com;
+
+import br.com.codewriter.CodeWriter;
+import br.com.parser.Parser;
+
+import java.nio.file.Path;
+
+public class VMTranslator {
+
+    public static void main(String[] args) throws Exception {
+
+        Path input = Path.of(args[0]);
+
+        String outputName = input.toString().replace(".vm", ".asm");
+
+        Parser parser = new Parser(input);
+        CodeWriter codeWriter = new CodeWriter(Path.of(outputName));
+
+        while (parser.hasMoreCommands()) {
+
+            parser.advance();
+
+            switch (parser.commandType()) {
+
+                case C_ARITHMETIC ->
+                    codeWriter.writeArithmetic(parser.arg1());
+
+                case C_PUSH ->
+                    codeWriter.writePush(
+                        parser.arg1(),
+                        parser.arg2()
+                    );
+
+                case C_POP ->
+                    codeWriter.writePop(
+                        parser.arg1(),
+                        parser.arg2()
+                    );
+            }
+        }
+
+        codeWriter.close();
+    }
+}
